1279df5649ce77be038e3a2ed15bac429b565443
João Nunes <joaovitorschafriva@gmail.com>
2023-07-08 09:42:31 -0300

    Inserindo dados padrões previamente

---
+++ Banco de dados/Insert Data.sql
@@ -0,0 +1,18 @@
+INSERT INTO status (tipo) VALUES ("Em produção");
+INSERT INTO status (tipo) VALUES ("Em entrega");
+INSERT INTO status (tipo) VALUES ("Concluido");
+
+INSERT INTO massas (tipo) VALUES ("Massa comum");
+INSERT INTO massas (tipo) VALUES ("Massa integral");
+INSERT INTO massas (tipo) VALUES ("Massa temperada");
+
+INSERT INTO bordas (tipo) VALUES ("Cheddar");
+INSERT INTO bordas (tipo) VALUES ("Catupiry");
+
+INSERT INTO sabores (nome) VALUES ("Frago com Catupiry");
+INSERT INTO sabores (nome) VALUES ("Calabresa");
+INSERT INTO sabores (nome) VALUES ("Lombinho");
+INSERT INTO sabores (nome) VALUES ("Filé com Cheddar");
+INSERT INTO sabores (nome) VALUES ("Portuguesa");
+INSERT INTO sabores (nome) VALUES ("Margherita");
+
