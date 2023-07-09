59e322715f147032309c07555c0e63134521d4c9
João Nunes <joaovitorschafriva@gmail.com>
2023-07-09 18:13:24 -0300

    Estilizando o cabeçalho

--- templates/header.php
+++ templates/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Faça seu Pedido!</title>
     <!-- Bootstrap -->
-    <!-   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- App CSS -->
@@ -14,5 +14,16 @@
 </head>
 <body>
     <header>
-        <p>Cabeçalho</p>
+        <nav class="navbar navbar-expand-lg">
+            <a href="index.php" class="navbar-brand">
+                <img src="img/pizza.svg" alt="Pizzaria do João" id="brand-logo">
+            </a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a href="index.php" class="nav-link">Peça sua pizza</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </header>
