2d750a78f880b1eec58da16637562a867a124540
João Nunes <joaovitorschafriva@gmail.com>
2023-07-11 20:11:57 -0300

    Finalizando tela de pedido

--- process/pizza.php
+++ process/pizza.php
@@ -29,8 +29,37 @@
             $_SESSION["msg"] = "Selecione no máximo 3 sabores!";
             $_SESSION["status"] = "warning";
         } else {
-            echo "passou da validação";
-            exit;
+
+            $stmt = $conn->prepare("INSERT INTO pizzas (borda_id, massa_id) VALUES (:borda, :massa)");
+
+            $stmt->bindParam(":borda", $borda, PDO::PARAM_INT);
+            $stmt->bindParam(":massa", $massa, PDO::PARAM_INT);
+
+            $stmt->execute();
+            
+            $pizzaId = $conn->lastInsertId();
+            
+            $stmt = $conn->prepare("INSERT INTO pizza_sabor (pizza_id, sabor_id) VALUES (:pizza, :sabor)");
+
+            foreach($sabores as $sabor) {
+                $stmt->bindParam(":pizza", $pizzaId, PDO::PARAM_INT);
+                $stmt->bindParam(":sabor", $sabor, PDO::PARAM_INT);
+                
+                $stmt->execute();
+
+            }
+
+            $stmt = $conn->prepare("INSERT INTO pedidos (pizza_id, status_id) VALUES (:pizza, :status)");
+
+            $statusID = 1;
+
+            $stmt->bindParam(":pizza", $pizzaId);
+            $stmt->bindParam(":status", $statusID);
+
+            $stmt->execute();
+
+            $_SESSION["msg"] = "Pedido realizado com sucesso";
+            $_SESSION["status"] = "success";
         }
 
         header("Location: ..");
